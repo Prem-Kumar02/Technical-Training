0927bca27cb9de8c0b91764a1d1c4fd3e1f55c16
Prem Kumar <singhpremsingh12345@gmail.com>
2025-10-29 11:59:12 +0530

    Solved Reverse the array

---
+++ Arrays/revtheArr.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+import java.util.* ;
+import java.util.ArrayList;
+
+public class revtheArr {
+    public static void reverseArray(ArrayList<Integer> arr, int m){
+        int start=m+1;
+        int end=arr.size()-1;
+        while(start<end){
+            int temp=arr.get(start);
+            arr.set(start,arr.get(end));
+            arr.set(end,temp);
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String args[]) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        int m = 4; // Index after which to reverse
+        reverseArray(arr, m);
+        System.out.println(arr); // Output the modified array
+    }
+}
+    
